bfc24541449f692e412695d27888a76b5b594e7f
neeladri22 <neeladri22@gmail.com>
2022-07-24 04:31:50 -0400

    Retrieve salary details and employee who joined particular date

--- Employee_Payroll_Problem.sql
+++ Employee_Payroll_Problem.sql
@@ -19,6 +19,25 @@ select * from employee_payroll
 --Inserting the data in to table
 INSERT INTO employee_payroll values ('Neeladri',30000,'2021-07-11'),('Usha',1500,'2017-02-10'),('Hema',40000,'2019-11-04'),('Raji',15000,'2020-06-20');
 
+--to select all / display the table
+select * from employee_payroll
+
+--Retrieve salary data from particular employee
+SELECT salary FROM employee_payroll
+WHERE name = 'Neeladri'
+
+--Retrieve Names from the selected date to present date
+select name FROM employee_payroll
+WHERE startdate BETWEEN CAST('2021-01-01' AS DATE) AND GETDATE()
+
+
+
+
+
+
+
+
+
 
 
 
